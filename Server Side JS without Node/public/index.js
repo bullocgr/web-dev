b3d79216fa909e66a3fe05d00b3afec43c289b49
Grace Bullock <bullocgr@flip2.engr.oregonstate.edu>
2019-01-22 18:57:47 -0800

    Cleaned up directory

---
+++ Server Side JS without Node/public/index.js
@@ -0,0 +1,19 @@
+/*
+ * This file doesn't contain any JS because this assignment is released before
+ * the due date of the previous assignment, so I don't want to give you the
+ * solution to that assignment yet ;)  You'll know this file was served
+ * correctly if you see the alert below in your browser.  Alternatively, you
+ * can add your own solution code from the previous assignment here in order
+ * to see all of the client-side JS interactions you implemented.
+ */
+
+/*
+ * Write your JS code in this file.  Don't forget to include your name and
+ * @oregonstate.edu email address below.
+ *
+ * Name: Grace Bullock
+ * Email: bullocgr@oregonstate.edu
+ */
+
+
+alert('Client-side JS was successfully loaded.');
